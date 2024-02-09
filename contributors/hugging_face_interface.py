13e5e691895cd2dd5011ff932c929203ce972899
twadeclark <115502590+twadeclark@users.noreply.github.com>
2024-02-09 15:51:03 -0800

    added process timer formatting and presentation improvements; badges and images

--- contributors/hugging_face_interface.py
+++ contributors/hugging_face_interface.py
@@ -1,4 +1,5 @@
 import json
+import random
 import requests
 from contributors.abstract_ai_unit import AbstractAIUnit
 
@@ -9,10 +10,10 @@ class HuggingFaceInterface(AbstractAIUnit):
 
     def fetch_inference(self, model, formatted_messages):
         headers = {"Authorization": f"Bearer {self.api_key}"}
-        this_url = self.base_url + model['model_name']
+        this_api_endpoint = self.base_url + model['model_name']
 
         def query(payload):
-            response = requests.post(this_url, headers=headers, json=payload, timeout=120, stream=True)
+            response = requests.post(this_api_endpoint, headers=headers, json=payload, timeout=120, stream=True)
             all_chunks = ""
 
             for chunk in response.iter_content(chunk_size=1):
@@ -20,6 +21,20 @@ class HuggingFaceInterface(AbstractAIUnit):
 
             response.close()
             return json.loads(all_chunks)
+
+        # flavors
+        max_tokens = random.randint(1, 20) * 25
+        temperature = random.uniform(0.0, 2.0) # range 0 and 2, Defaults to 1
+        frequency_penalty = random.uniform(-2.0, 2.0) # range -2.0 and 2.0, Defaults to 0
+        presence_penalty = random.uniform(-2.0, 2.0) # range -2.0 and 2.0, Defaults to 0
+
+        max_tokens_as_string = str(max_tokens)
+        temperature_as_string = "{:.1f}".format(temperature)
+        frequency_penalty_as_string = "{:.1f}".format(frequency_penalty)
+        presence_penalty_as_string = "{:.1f}".format(presence_penalty)
+
+        flavors = f" \t max_tokens: {max_tokens_as_string}, \t temperature: {temperature_as_string}, \t frequency_penalty: {frequency_penalty_as_string}, \t presence_penalty: {presence_penalty_as_string}"
+        print(flavors)
 
         data = query(
             {
@@ -64,15 +79,15 @@ class HuggingFaceInterface(AbstractAIUnit):
     def find_keys(self, json_input, target_keys):
         results = {}
 
-        def _find_keys(json_fragment):
+        def _find_keys_recursive(json_fragment):
             if isinstance(json_fragment, dict):
                 for key, value in json_fragment.items():
                     if key in target_keys:
                         results[key] = value
-                    _find_keys(value)
+                    _find_keys_recursive(value)
             elif isinstance(json_fragment, list):
                 for item in json_fragment:
-                    _find_keys(item)
+                    _find_keys_recursive(item)
 
-        _find_keys(json_input)
+        _find_keys_recursive(json_input)
         return results
