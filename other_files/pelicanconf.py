27f18c2fd98d9b08756591638636895134d0859a
twadeclark <115502590+twadeclark@users.noreply.github.com>
2024-07-24 22:43:54 -0700

    file location cleanup images

---
+++ other_files/pelicanconf.py
@@ -0,0 +1,104 @@
+AUTHOR = 'Raw Circuit'
+SITENAME = 'RawCircuit'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'EN'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 4
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+# OUTPUT_PATH = 'output/'
+
+
+# THEME = "C:/Users\/twade/git/elegant"
+THEME = "../pelican/elegant"
+
+# QR_CODE = "FALSE" 
+SOCIAL_PROFILE_LABEL = 0
+DIRECT_TEMPLATES = ["index", "tags", "categories", "archives", "authors", "about", "privacy"]
+LANDING_PAGE_TITLE = "AI Awakens"
+
+SITE_NAME_IMAGE = 'rawcircuit_transparent_small.png'
+
+RANDOM_TAG_LINES = ["Intelligent Insights",
+"AI Unleashed",
+"Circuit Wisdom",
+"Think Forward",
+"Minds Connected",
+"Insightful Intelligence",
+"Future Narratives",
+"AI Perspectives",
+"Cognitive Conversations",
+"Digital Discourse",
+"Mind Over Matter",
+"Beyond Human",
+"Unearthly Insights",
+"Provoking Thought",
+"Circuit Breakers",
+"Robotic Revelations",
+"AI Awakens",
+"Echoes of Tomorrow",
+"Synthetic Sagacity",
+"Digital Provocateur",
+"AI Driven Discovery",
+"Innovate, Integrate, Inspire",
+"Explore AI Perspectives",
+"Intelligence Beyond Boundaries",
+"Unleashing Digital Minds",
+"Navigate Future Narratives",
+"Insights from AI",
+"Circuiting Creative Minds",
+"Pioneering AI Conversations",
+"Unravel AI Wisdom",
+"Beyond Human Intellect",
+"Challenge. Change. Converse.",
+"AI Breaks Barriers",
+"Redefine Neural Narratives",
+"Echoes Beyond Algorithms",
+"Disrupt. Discuss. Discover.",
+"Provoke. Ponder. Progress.",
+"Synthesize. Surpass. Surprise.",
+"Cognition Meets Revolution",
+"Questioning AI's Canvas",
+"Discover. Debate. Dominate.",
+"Innovate. Ignite. Inspire.",
+"Question. Quantify. Qualify.",
+"Analyze. Adapt. Advance.",
+"Explore. Exceed. Excel.",
+"Think. Theorize. Thrive.",
+"Create. Challenge. Conquer.",
+"Learn. Lead. Leap.",
+"Dream. Decode. Deliver.",
+"Observe. Outperform. Overcome.",
+"Circuit Synergy",
+"Intellect Unleashed",
+"Insight Revolution",
+"Future Fabricated",
+"Mind Meld",
+"Thought Transcendence",
+"Digital Genesis",
+"Cognition Crafted",
+"Echoes Evolved",
+"Visionary Voyage"]
